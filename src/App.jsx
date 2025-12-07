5980b0781df4225ec4d6bd49ece1cb26362e0565
jarretphon <jarretphon@gmail.com>
2025-12-07 19:00:30 +0800

    Create promptCard UI component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
 //import './App.css'
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
-import { Box } from '@mui/material'
-import Nav from './Nav.jsx'
-import PromptCard from './PromptCard.jsx'
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Box } from "@mui/material";
+import Nav from "./Nav.jsx";
+import PromptCard from "./PromptCard.jsx";
 
 function App() {
   return (
-    <Box>
+    <Box sx={{ width: "100%", p: 0 }}>
       <Nav />
-      <PromptCard />
+      <PromptCard
+        title="Title 1"
+        description="Description 1"
+        content={`Content 1
+          Line 2 
+          Line 3
+          Line 4
+          Line 5
+          Line 6`}
+      />
+      <PromptCard
+        title="Title 2"
+        description="Description 2"
+        content="Content 2"
+      />
     </Box>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/PromptCard.jsx
+++ src/PromptCard.jsx
@@ -1,48 +1,140 @@
-import React, { useState } from 'react'
-import { Card, CardActions, CardContent, CardHeader, IconButton, Typography } from '@mui/material'
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { useState } from "react";
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Collapse,
+  IconButton,
+  styled,
+  Typography,
+} from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-function PromptCard() {
+export default function PromptCard({ title, description, content }) {
+  const [previewed, setPreviewed] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
-    const [expanded, setExpanded] = useState(false);
+  const handleExpandClick = (e) => {
+    e.stopPropagation();
+    setExpanded(!expanded);
+  };
 
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    }
-
-    return (
-        <Card>
-            <CardHeader 
-                title="Prompt Title" 
-                subheader="Brief description of the prompt"
-                slotProps={{
-                    title: {variant: 'body1'},
-                    subheader: {variant: 'body2'}
-                }}
-                action={
-                    <CardActions disableSpacing>
-                        <IconButton size='small'>
-                            <MoreVertIcon fontSize='small'/>
-                        </IconButton>
-                    </CardActions>
-                }
-                sx={{pb: 0.5}}
-            />
-            <CardContent sx={{p: 1, pt: 1.5, pb: 1.5, m: 2, mt: 0, borderRadius: 2, backgroundColor: '#f3f3f3'}}>
-                <Typography variant="body2" color="textSecondary">
-                    Template of prompt goes here. 
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                    This is where the main content of the prompt card will be displayed.
-                    There will be many lines here
-                    many lines
-                    many lines
-                    many lines
-                </Typography>
-            </CardContent>
-        </Card>
-    )
+  return (
+    <Card
+      onMouseEnter={() => setPreviewed(true)}
+      onMouseLeave={() => {
+        setPreviewed(false);
+        setExpanded(false);
+      }}
+    >
+      <CardHeader
+        title={title}
+        subheader={description}
+        slotProps={{
+          title: { variant: "body1" },
+          subheader: { variant: "body2" },
+        }}
+        action={
+          <CardActions disableSpacing>
+            <IconButton size="small">
+              <MoreVertIcon />
+            </IconButton>
+          </CardActions>
+        }
+      />
+      <Collapse in={previewed} timeout="auto" unmountOnExit>
+        <StyledCardContent>
+          <Box
+            sx={{ maxHeight: expanded ? "none" : 100 }}
+            onClick={() => alert("clicked")}
+          >
+            <StyledContentText>{content}</StyledContentText>
+            {!expanded && <BottomFade aria-hidden />}
+            <ExpandMore
+              expand={expanded}
+              onClick={(e) => handleExpandClick(e)}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </ExpandMore>
+          </Box>
+        </StyledCardContent>
+      </Collapse>
+    </Card>
+  );
 }
 
-export default PromptCard
+const ExpandMore = styled((props) => {
+  const { ...other } = props;
+  return <IconButton size="small" {...other} />;
+})(({ theme }) => ({
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+  variants: [
+    {
+      props: ({ expand }) => !expand,
+      style: { transform: "translateX(-50%) rotate(0deg)" },
+    },
+    {
+      props: ({ expand }) => !!expand,
+      style: { transform: "translateX(-50%) rotate(180deg)" },
+    },
+  ],
+  position: "absolute",
+  left: "50%",
+  bottom: 0,
+  zIndex: 2000,
+  pointerEvents: "auto",
+}));
+
+const StyledCardContent = styled(CardContent)(({ theme }) => ({
+  paddingVertical: theme.spacing(2),
+  paddingHorizontal: theme.spacing(1.5),
+  margin: theme.spacing(2),
+  marginTop: theme.spacing(0),
+  borderRadius: theme.shape.borderRadius * 2,
+  overflow: "hidden",
+  backgroundColor: theme.palette.grey[100],
+  position: "relative",
+  textOverflow: "ellipsis",
+  cursor: "pointer",
+  "&:hover": {
+    backgroundColor: theme.palette.grey[200],
+  },
+  "&:active": {
+    backgroundColor: theme.palette.grey[300],
+  },
+  transition: theme.transitions.create("background-color", {
+    duration: theme.transitions.duration.short,
+  }),
+}));
+
+const BottomFade = styled((props) => {
+  return <Box {...props} />;
+})(({ theme }) => ({
+  position: "absolute",
+  left: 0,
+  right: 0,
+  bottom: 0,
+  height: "4em",
+  pointerEvents: "none",
+  zIndex: 1500,
+  borderBottomRadius: theme.shape.borderRadius * 2,
+  background: `linear-gradient(180deg, rgba(245,245,245,0) 0%, ${
+    theme.palette.mode === "dark" ? "rgba(18,18,18,0.9)" : "rgba(245,245,245,1)"
+  } 90%)`,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+}));
+
+const StyledContentText = styled(Typography)(({ theme }) => ({
+  whiteSpace: "pre-line",
+  variant: theme.typography.body2,
+  color: theme.palette.text.secondary,
+}));
